3c06180cbd5b1fafd08d6e61b870d95512e90139
AzatOmarov <kst369@mail.ru>
2019-10-06 05:37:45 +0200

    Check for the expired token in App container

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
 import {Provider} from 'react-redux';
 import store from './store';
 import Navbar from './components/layout/Navbar';
@@ -22,6 +22,15 @@ if(localStorage.jwtToken){
   const decoded = jwt_decode(localStorage.jwtToken);
   // Set user and isAuthenticated
   store.dispatch(setCurrentUser(decoded));
+  // Check for the expired token
+  const currentTime = Date.now()/1000;
+  if(decoded.exp < currentTime){
+    //Logout User
+    store.dispatch(logoutUser());
+    // TODO: clear profile
+    //Redirect to login
+    window.location.href = '/login';
+  }
 }
 
 function App() {
