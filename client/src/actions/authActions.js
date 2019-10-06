df6807a32bf2e601445e484d4c7bf37be8ec68e0
AzatOmarov <kst369@mail.ru>
2019-10-06 05:14:42 +0200

    Create logoutUser action

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -46,4 +46,14 @@ export const setCurrentUser = (decoded) => {
 	}
 }
 
+// Log user out 
+export const logoutUser = () => dispatch => {
+	// Remove token from localstorage
+	localStorage.removeItem('jwtToken');
+	// Remove auth header for future requests
+	setAuthToken(false);
+	// Set current user to empty object which will set isAuthenticated to false
+	dispatch(setCurrentUser({}));
+}
+
 
